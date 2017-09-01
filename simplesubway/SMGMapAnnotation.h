0983f2b39fdf13687c49310d0e6a7b7807a47c0f
SuperMarcus <marcus.skye.lewis@gmail.com>
2017-08-31 20:17:12 -0400

    reconfigured for longpress, new maps

---
+++ simplesubway/SMGMapAnnotation.h
@@ -0,0 +1,13 @@
+//
+//  SMGMapAnnotation.h
+//  simplesubway
+//
+//  Created by Marcus Skye Lewis on 7/12/14.
+//  Copyright (c) 2014 Marcus Lewis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SMGMapAnnotation : NSObject
+
+@end
---
+++ simplesubway/SMGMapAnnotation.m
@@ -0,0 +1,13 @@
+//
+//  SMGMapAnnotation.m
+//  simplesubway
+//
+//  Created by Marcus Skye Lewis on 7/12/14.
+//  Copyright (c) 2014 Marcus Lewis. All rights reserved.
+//
+
+#import "SMGMapAnnotation.h"
+
+@implementation SMGMapAnnotation
+
+@end
